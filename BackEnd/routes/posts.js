6f77ef26ff1007da572b70cc9096860495318322
KianPaliani <kpaliani@uwo.ca>
2016-12-01 18:52:55 -0500

    fixed studentDB

--- BackEnd/routes/posts.js
+++ BackEnd/routes/posts.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var models = require('../models/courseBlogDB');
+var models = require('../models/studentDB');
 var bodyParser = require('body-parser');
 var parseUrlencoded = bodyParser.urlencoded({extended: false});
 var parseJSON = bodyParser.json();
