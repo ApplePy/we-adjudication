08a4a53ced6ef663967022bca06f3f89a0675361
Darryl Murray <darrylmurray@me.com>
2016-12-02 12:18:37 -0500

    Set up more routes.

--- BackEnd/routes/students.js
+++ BackEnd/routes/students.js
@@ -1,24 +1,22 @@
+"use strict";
+
 var express = require('express');
 var router = express.Router();
 var models = require('../models/studentDB');
 
 router.route('/')
-    // .post(parseUrlencoded, parseJSON, function (request, response) {
-    //     var post = new models.Posts(request.body.post);
-    //     post.save(function (error) {
-    //         if (error) response.send(error);
-    //         response.json({post: post});
-    //     });
-    //
-    //
-    // })
-
     // TODO: Get all students
     .get(function (request, response) {
             models.Student.find(function (error, posts) {
                 if (error) response.send(error);
                 response.json({post: posts});
             });
+    })
+    .get('/first', function(request, response) {
+
+    })
+    .get('/last', function(request, response) {
+
     });
 
 router.route('/:studentNo')
@@ -53,14 +51,11 @@ router.route('/:studentNo')
             }
         });
     })
-    // .delete(parseUrlencoded, parseJSON, function (request, response) {
-    //     models.Posts.findByIdAndRemove(request.params.post_id,
-    //         function (error, deleted) {
-    //             if (!error) {
-    //                 response.json({post: deleted});
-    //             }
-    //         }
-    //     );
-    // });
+    .get('/next', function (request, response) {
+
+    })
+    .get('/previous', function(request, response) {
+
+    });
 
 module.exports = router;
