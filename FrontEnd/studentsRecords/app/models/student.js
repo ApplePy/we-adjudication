666c3e50d712f5cd5c9087c12dff33dac5fcb546
Darryl Murray <darrylmurray@me.com>
2017-02-01 08:35:37 -0500

    Forgot to add new properties to Student on Front End

--- FrontEnd/studentsRecords/app/models/student.js
+++ FrontEnd/studentsRecords/app/models/student.js
@@ -7,6 +7,10 @@ export default DS.Model.extend({
   gender: DS.attr('number'),
   DOB: DS.attr('date'),
   photo: DS.attr(),
+  registrationComments: DS.attr('string'),
+  basisOfAdmission: DS.attr('string'),
+  admissionAverage: DS.attr('number'),
+  admissionComments: DS.attr('string'),
   resInfo: DS.belongsTo('residency')
 
 });
