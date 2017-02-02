6b69275dde2559dd29033446e43a33c6e724c464
Darryl Murray <darrylmurray@me.com>
2017-02-02 16:46:01 -0500

    Updated residencies comments and improved the query mechanism

--- BackEnd/studentsRecords/routes/residencies.js
+++ BackEnd/studentsRecords/routes/residencies.js
@@ -6,35 +6,59 @@ var parseUrlencoded = bodyParser.urlencoded({extended: false});
 var parseJSON = bodyParser.json();
 
 router.route('/')
+    // Post a new residency
     .post(parseUrlencoded, parseJSON, function (request, response) {
+        // The body of the request will be { residency: {*content} }
         var residency = new models.Residencies(request.body.residency);
+
         residency.save(function (error) {
             if (error) response.status(500).send(error);
             else response.json({residency: residency});
         });
     })
+
+    // Get all residencies, or the residency that a student belongs to
     .get(parseUrlencoded, parseJSON, function (request, response) {
-        var Student = request.query.filter;
-        if (!Student) {
+        // Get query object
+        var filter = request.query.filter;
+
+        // If no query, return all
+        if (!filter) {
             models.Residencies.find(function (error, residencies) {
                 if (error) response.status(500).send(error);
                 else response.json({residency: residencies});
             });
-        } else {
-            models.Residencies.find({"students": {$in: [Student.student]}}, function (error, students) {
+        }
+
+        // A query was submitted for the residency of a student
+        else if (filter.student) {
+            models.Residencies.find({"students": {$in: [filter.student]}}, function (error, students) {
+                if (error) response.status(500).send(error);
+                else response.json({residency: students});
+            });
+        }
+
+        // A query was submitted for a specific residency name
+        else if (filter.name) {
+            models.Residencies.find({"name": filter.name}, function (error, students) {
                 if (error) response.status(500).send(error);
                 else response.json({residency: students});
             });
         }
     });
 
+// Manipulate residency by its Mongo id
 router.route('/:residency_id')
+
+    // Get a specific residency by Mongo id
     .get(parseUrlencoded, parseJSON, function (request, response) {
         models.Residencies.findById(request.params.residency_id, function (error, residency) {
             if (error) response.status(404).send(error);
             else response.json({residency: residency});
         })
     })
+
+    // Update a specific residency
     .put(parseUrlencoded, parseJSON, function (request, response) {
         models.Residencies.findById(request.params.residency_id, function (error, residency) {
             if (error) {
