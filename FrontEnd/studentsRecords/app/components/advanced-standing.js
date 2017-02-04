21b10e1f9bfe3ed885a908e4b3d50e6bb8e54ec3
Eiley Tarlton <etarlton@uwo.ca>
2017-02-04 18:32:46 -0500

    delete advance standing and award

--- FrontEnd/studentsRecords/app/components/advanced-standing.js
+++ FrontEnd/studentsRecords/app/components/advanced-standing.js
@@ -7,5 +7,17 @@ export default Ember.Component.extend({
     grade: null,
     from: null, */
     recipient: null,
-    standing: null
+    standing: null,
+     store: Ember.inject.service(),
+  
+ actions: {
+  deleteStanding() {
+    //alert(this.get('award').id);
+    this.get('store').findRecord('advanced-standing', this.get('standing').id, { backgroundReload: false }).then(function(standing) {
+        standing.deleteRecord();
+        standing.get('isDeleted');
+        standing.save();
+      });
+  }
+ }
 });
--- FrontEnd/studentsRecords/app/components/scholarships-awards.js
+++ FrontEnd/studentsRecords/app/components/scholarships-awards.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  award: null
+   store: Ember.inject.service(),
+  award: null,
+ actions: {
+  deleteAward() {
+    //alert(this.get('award').id);
+    this.get('store').findRecord('award', this.get('award').id, { backgroundReload: false }).then(function(award) {
+        award.deleteRecord();
+        award.get('isDeleted');
+        award.save();
+      });
+  }
+ }
 });
