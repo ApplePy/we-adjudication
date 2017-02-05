64fd1796d86a80c82f695352dac28e9f79b6a8e3
Darryl Murray <darrylmurray@me.com>
2017-02-04 22:49:50 -0500

    Update studentsRecordsDB.js to fix bugs.

--- BackEnd/studentsRecords/models/studentsRecordsDB.js
+++ BackEnd/studentsRecords/models/studentsRecordsDB.js
@@ -31,10 +31,12 @@ var studentsSchema = mongoose.Schema(
         lastName: String,
         DOB: Date,
         photo: String,
+        registrationComments: String,
+        basisOfAdmission: String,
         admissionAverage: Number,
         admissionComments: String,
         resInfo: {type: mongoose.Schema.ObjectId, ref: 'Residencies'},
-        genderInfo: {type: mongoose.Schema.ObjectId, ref: 'Genders'}
+        genderInfo: {type: mongoose.Schema.ObjectId, ref: 'Genders'},
         awards: [{type: mongoose.Schema.ObjectId, ref: 'Awards'}],
         advancedStandings: [{type: mongoose.Schema.ObjectId, ref: 'AdvancedStandings'}]
     }
