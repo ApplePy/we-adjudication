e9d2bbbcd648791fc18bbe9a4db1ae28c45f515b
Darryl Murray <darrylmurray@me.com>
2017-02-06 11:08:28 -0500

    Moved backend api to /api namespace and added some edits to port during production mode.

--- BackEnd/studentsRecords/server.js
+++ BackEnd/studentsRecords/server.js
@@ -11,7 +11,10 @@ var advancedStandings = require('./routes/advancedStandings');
 
 
 app.use(function (request, response, next) {
-    response.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    // Leave the dual server system in place for everything except production
+    if (process.env.NODE_ENV !== 'production') {
+        response.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    }
     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     response.header('Access-Control-Allow-Methods', 'POST, PATCH, GET, PUT, DELETE, OPTIONS');
     next();
@@ -21,20 +24,30 @@ app.use(function (request, response, next) {
 if (process.env.NODE_ENV !== 'test') {
     app.use(logger);
 }
-//app.use(express.static('public'));
 
-// Set json header
-app.use((req, res, next)=>{res.setHeader('Content-Type', 'application/json'); next();});
+// Set API namespace
+var api = express.Router();
+app.use('/api', api);
 
-app.use('/students', students);
-app.use('/residencies', residencies);
-app.use('/genders', genders);
-app.use('/awards', awards);
-app.use('/advancedStandings', advancedStandings);
+// Set json header and API routes
+api.use((req, res, next)=>{res.setHeader('Content-Type', 'application/json'); next();});
+api.use('/students', students);
+api.use('/residencies', residencies);
+api.use('/genders', genders);
+api.use('/awards', awards);
+api.use('/advancedStandings', advancedStandings);
 
+// Set default serve
+if (process.env.NODE_ENV === 'production') app.use(express.static('dist'));
 
-app.listen(3700, function () {
-    console.log('Listening on port 3700');
+// Port to listen on
+var port = 3700;
+
+// Change port to 80 in production
+if (process.env.NODE_ENV === 'production') port = 80;
+
+app.listen(port, function () {
+    console.log('Listening on port ' + port);
 });
 
 module.exports = app;   // For supporting tests
