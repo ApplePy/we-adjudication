62a3aa24dfa5e3a144232896d7664776e8d30fe2
KianPaliani <kpaliani@uwo.ca>
2017-02-09 16:15:27 -0500

    validate gender and residency manage system code inputs.  Cleaned up a bit also

--- FrontEnd/studentsRecords/app/components/manage-system.js
+++ FrontEnd/studentsRecords/app/components/manage-system.js
@@ -7,8 +7,6 @@ export default Ember.Component.extend({
   residencies: null,
   newGender: null,
   newResidency: null,
-  //genderNames: [],
-  //residencyNames: [],
 
   init() {
 
@@ -29,30 +27,33 @@ export default Ember.Component.extend({
   actions: {
 
     addGender () {
-      var gender = this.get('store').createRecord('gender', {
-        name: this.get('newGender')
-      });
+      if (this.get('newGender') != "") {
+        var gender = this.get('store').createRecord('gender', {
+          name: this.get('newGender')
+        });
 
-      gender.save().then(function() {
-        console.log("Added Gender");
-      }, function() {
-        console.log("Could not add gender");
-      });
+        gender.save().then(function() {
+          console.log("Added Gender");
+        }, function() {
+          console.log("Could not add gender");
+        });
+      }
     },
 
     modifyGender (genderId, index){
 
-      //var newGenderName = Ember.$("#" + index + ", .genderInput").val();
       var newGenderName = Ember.$("#" + index).val();
 
-      this.get('store').findRecord('gender', genderId).then(function(gender) {
-        gender.set('name', newGenderName);
-        gender.save().then(function() {
-          console.log("Modified Gender");
-        }, function() {
-          console.log("Could not modify gender");
+      if (newGenderName != "") {
+        this.get('store').findRecord('gender', genderId).then(function(gender) {
+          gender.set('name', newGenderName);
+          gender.save().then(function() {
+            console.log("Modified Gender");
+          }, function() {
+            console.log("Could not modify gender");
+          });
         });
-      });
+      }
     },
 
     deleteGender (genderId){
@@ -64,33 +65,36 @@ export default Ember.Component.extend({
     },
 
     addResidency () {
-      var residency = this.get('store').createRecord('residency', {
-        name: this.get('newResidency')
-      });
+      if (this.get('newResidency') != "") {
+        var residency = this.get('store').createRecord('residency', {
+          name: this.get('newResidency')
+        });
 
-      residency.save().then(function() {
-        console.log("Added Residency");
-      }, function() {
-        console.log("Could not add residency");
-      });
+        residency.save().then(function() {
+          console.log("Added Residency");
+        }, function() {
+          console.log("Could not add residency");
+        });
+      }
     },
 
     modifyResidency (residencyId, index){
 
-      //var newResidencyName = Ember.$("#" + index + ", .residencyInput").val();
       var newResidencyName = Ember.$("#" + index).val();
       console.log(residencyId);
       console.log(index);
       console.log(newResidencyName);
 
-      this.get('store').findRecord('residency', residencyId).then(function(residency) {
-        residency.set('name', newResidencyName);
-        residency.save().then(function() {
-          console.log("Modified Residency");
-        }, function() {
-          console.log("Could not modify residency");
+      if (newResidencyName != "") {
+        this.get('store').findRecord('residency', residencyId).then(function(residency) {
+          residency.set('name', newResidencyName);
+          residency.save().then(function() {
+            console.log("Modified Residency");
+          }, function() {
+            console.log("Could not modify residency");
+          });
         });
-      });
+      }
     },
 
     deleteResidency (residencyId){
