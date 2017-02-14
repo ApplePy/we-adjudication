8e5738fec9601486952500b65f5fd0f22b8842c5
Eiley Tarlton <etarlton@uwo.ca>
2017-02-13 19:21:48 -0500

    advanced standings and awards are on one line

--- FrontEnd/studentsRecords/app/components/scholarships-awards.js
+++ FrontEnd/studentsRecords/app/components/scholarships-awards.js
@@ -3,9 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
    store: Ember.inject.service(),
   award: null,
+  updateAward: null,
+
  actions: {
   deleteAward() {
-    //alert(this.get('award').id);
     this.get('store').findRecord('award', this.get('award').id, { backgroundReload: false }).then(function(award) {
         award.deleteRecord();
         award.get('isDeleted');
@@ -14,12 +15,8 @@ export default Ember.Component.extend({
   },
 
    updateAward() {
-   // alert("1 "+ this.get('standing.course'));
-    this.get('store').findRecord('award', this.get('award').id).then((award) => {
-        award.set('note', this.get('award.note'));
-        award.save();
-      });
-      
+       this.set('updateAward', true); 
   }
+
  }
 });
