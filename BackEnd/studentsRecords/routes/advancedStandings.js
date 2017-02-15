63b96c350b1d666c1664974707b8f24776b85b5d
Darryl Murray <darrylmurray@me.com>
2017-02-15 15:35:39 -0500

    Revert "Revert "Refactored routes into a generic route structure that is customized and extended in a Setup function.""
    This reverts commit 3751d73a27c28d065ee7113bf3291a6ec556bd16.

--- BackEnd/studentsRecords/routes/advancedStandings.js
+++ BackEnd/studentsRecords/routes/advancedStandings.js
@@ -1,89 +1,21 @@
-var express = require('express');
-var router = express.Router();
 var AdvancedStandings = require('../models/schemas/studentinfo/advancedStandingSchema');
-var bodyParser = require('body-parser');
-var parseUrlencoded = bodyParser.urlencoded({extended: false});
-var parseJSON = bodyParser.json();
+var Setup = require('./genericRouting');
 
-router.route('/')
 
-    // New advanced standing entry
-    .post(parseUrlencoded, parseJSON, function (request, response) {
-        var advancedStanding = new AdvancedStandings(request.body.advancedStanding);
-
-        if (!advancedStanding.recipient) response.status(400).json({error: {message: "Recipient must be specified."}});
-        else advancedStanding.save(function (error) {
-            if (error) response.status(500).send(error);
-            else response.status(201).json({advancedStanding: advancedStanding});
-        });
-    })
-
-    // Get advanced standings
-    .get(parseUrlencoded, parseJSON, function (request, response) {
-        var Student = request.query.filter;
-
-        // Get all advanced standings
-        if (!Student) {
-            AdvancedStandings.find(function (error, advancedStanding) {
-                if (error) response.status(500).send(error);
-                response.json({advancedStanding: advancedStanding});
-            });
-        }
-        // Get advanced standings for a student
-        else {
-            AdvancedStandings.find({"recipient": Student.recipient}, function (error, students) {
-                if (error) response.status(500).send(error);
-                response.json({advancedStanding: students});
-            });
-        }
-    });
-
-router.route('/:advancedStanding_id')
-
-    // Get advanced standing by id
-    .get(parseUrlencoded, parseJSON, function (request, response) {
-        AdvancedStandings.findById(request.params.advancedStanding_id, function (error, advancedStanding) {
-            if (error) response.status(404).send(error);
-            else response.json({advancedStanding: advancedStanding});
-        })
-    })
-
-    // Update advanced standing
-    .put(parseUrlencoded, parseJSON, function (request, response) {
-        AdvancedStandings.findById(request.params.advancedStanding_id, function (error, advancedStanding) {
-            if (error) {
-                response.status(404).send({error: error});
-            }
-            else {
-                advancedStanding.course = request.body.advancedStanding.course;
-                advancedStanding.description = request.body.advancedStanding.description;
-                advancedStanding.grade = request.body.advancedStanding.grade;
-                advancedStanding.from = request.body.advancedStanding.from;
-                advancedStanding.units = request.body.advancedStanding.units;
-                advancedStanding.recipient = request.body.advancedStanding.recipient;
-
-                if (!advancedStanding.recipient) response.status(400).json({error: {message: "Recipient must be specified."}});
-                else advancedStanding.save(function (error) {
-                    if (error) {
-                        // Ends up here if the recipient specified is bad
-                        response.status(500).send({error: error});
-                    }
-                    else {
-                        response.json({advancedStanding: advancedStanding});
-                    }
-                });
-            }
-        });
-    })
-
-    // Delete award
-    .delete(parseUrlencoded, parseJSON, function (request, response) {
-        AdvancedStandings.findByIdAndRemove(request.params.advancedStanding_id,
-            function (error, deleted) {
-                if (error) response.status(500).send({error: error});
-                else response.json({advancedStanding: deleted});
-            }
-        );
-    });
-
-module.exports = router;
+module.exports =
+    Setup(
+        AdvancedStandings,
+        'advancedStanding',
+        false,
+        (req, res, mod) => {
+            if (!mod.recipient)
+                return ["Recipient must be specified"];
+            else
+                return 0
+        },
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined
+    );
