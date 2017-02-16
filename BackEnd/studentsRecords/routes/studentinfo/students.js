aa08c638ceca69343513d85a0840008d913f1839
Darryl Murray <darrylmurray@me.com>
2017-02-16 17:03:43 -0500

    Found a bug in students.js delete function.

--- BackEnd/studentsRecords/routes/studentinfo/students.js
+++ BackEnd/studentsRecords/routes/studentinfo/students.js
@@ -33,14 +33,16 @@ module.exports =
                 AdvancedStandings.remove({recipient: deleted._id}, (err2, removed2) => {
                     // Remove high school grades
                     HSGrades.remove({recipient: deleted._id}, (err3, removed3) => {
-                        // Remove university grades
-                        Grades.remove({student: deleted._id}, (err4, grades) => {
+                        // Find university grades and their records to delete
+                        Grades.find({student: deleted._id}, (err4, grades) => {
                             if (err) return;    // Just silently fail
+
                             // Get program records
                             let programIds = grades.map((el, idx, arr) => el.level);
 
-                            // Delete program records
+                            // Delete program records and grades
                             ProgramRecords.remove({_id: {$in: programIds}});
+                            Grades.remove({student: deleted._id});
                         });
                     });
                 });
