85f97831680b3f89f71e8e801f1b604a8057e764
mmurra88 <mmurra88@uwo.ca>
2017-02-16 20:28:34 -0500

    Can add any number of plans and delete as needed

--- FrontEnd/studentsRecords/app/components/add-program-record.js
+++ FrontEnd/studentsRecords/app/components/add-program-record.js
@@ -2,8 +2,41 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   notDONE: null,
+  genderModel: null,
+  residencyModel: null,
+  newNote: null,
+  plans: [],
+  hasPlan: false,
 
   actions:{
+    selectRecord(){
+
+    },
+
+    selectCourse(){
+
+    },
+
+    selectTerm(){
+
+    },
+
+    selectPlan(){
+
+    },
+
+    deletePlanField(){
+      this.get('plans').popObject();
+      if(this.get('plans').length === 0){
+        this.set('hasPlan', false);
+      }
+    },
+
+    newPlanField(){
+      this.get('plans').pushObject("newPlan");
+      this.set('hasPlan', true);
+    },
+
     close(){
       this.set('notDONE', false);
       Ember.$('.ui.modal').modal('hide');
