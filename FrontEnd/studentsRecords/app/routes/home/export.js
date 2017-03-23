d2829863d6e570e16e7ae2d2a77c0c773b781392
KianPaliani <kpaliani@uwo.ca>
2017-03-23 15:49:52 -0400

    copied and pasted js code into export route

--- FrontEnd/studentsRecords/app/routes/home/export.js
+++ FrontEnd/studentsRecords/app/routes/home/export.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+	store: Ember.inject.service(),
+
+	beforeModel() {
+		this.get('store').findAll('adjudication').then(adjudications => {
+			console.log("here");
+			console.log(adjudications);
+			if (adjudications.get('length') === 0) {
+				Ember.$('.ui.modal')
+  					.modal('setting', 'closable', false)
+  					.modal('show')
+				;
+				this.transitionTo('home');
+			}
+		});	
+  	}
+
 });
